e3b9dc03c7a13e72ab62d6683e7fe770e775aa31
EleKtr1X <aarnav.thite@gmail.com>
2024-06-03 11:10:23 -0400

    add spotify oauth recognition

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script lang='ts'>
+  import { nanoid } from 'nanoid';
+  
+  const state = nanoid();
+  const scope = 'user-library-read playlist-modify-public'
+  const params = new URLSearchParams()
+  params.append('response_type', 'code')
+  params.append('client_id', 'dab5276ca4ba46f4b634a28ef8965e5a')
+  params.append('redirect_uri', 'http://localhost:5173/playlist-generator/generate')
+  params.append('state', state)
+  params.append('scope', scope)
+
+  const url = 'https://accounts.spotify.com/authorize?' + params.toString()
+</script>
+
 <h1>Spotify Playlist Generator</h1>
-<a href='/playlist-generator/generate'><button>Log in with Spotify</button></a>
+<a href={url}><button>Log in with Spotify</button></a>
--- src/routes/generate/+page.svelte
+++ src/routes/generate/+page.svelte
@@ -1,1 +1,6 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  console.log($page.url.searchParams.get('code'))
+</script>
+
 <h1>Generate</h1>
