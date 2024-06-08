e9a0f966104a440166e8e5c05f85ee163c19a8c8
EleKtr1X <aarnav.thite@gmail.com>
2024-06-07 20:51:32 -0400

    add a bunch of stuff wow

--- src/api/gpt.ts
+++ src/api/gpt.ts
@@ -1,138 +1,7 @@
-const genres = [
-  "acoustic",
-  "afrobeat",
-  "alt-rock",
-  "alternative",
-  "ambient",
-  "anime",
-  "black-metal",
-  "bluegrass",
-  "blues",
-  "bossanova",
-  "brazil",
-  "breakbeat",
-  "british",
-  "cantopop",
-  "chicago-house",
-  "children",
-  "chill",
-  "classical",
-  "club",
-  "comedy",
-  "country",
-  "dance",
-  "dancehall",
-  "death-metal",
-  "deep-house",
-  "detroit-techno",
-  "disco",
-  "disney",
-  "drum-and-bass",
-  "dub",
-  "dubstep",
-  "edm",
-  "electro",
-  "electronic",
-  "emo",
-  "folk",
-  "forro",
-  "french",
-  "funk",
-  "garage",
-  "german",
-  "gospel",
-  "goth",
-  "grindcore",
-  "groove",
-  "grunge",
-  "guitar",
-  "happy",
-  "hard-rock",
-  "hardcore",
-  "hardstyle",
-  "heavy-metal",
-  "hip-hop",
-  "holidays",
-  "honky-tonk",
-  "house",
-  "idm",
-  "indian",
-  "indie",
-  "indie-pop",
-  "industrial",
-  "iranian",
-  "j-dance",
-  "j-idol",
-  "j-pop",
-  "j-rock",
-  "jazz",
-  "k-pop",
-  "kids",
-  "latin",
-  "latino",
-  "malay",
-  "mandopop",
-  "metal",
-  "metal-misc",
-  "metalcore",
-  "minimal-techno",
-  "movies",
-  "mpb",
-  "new-age",
-  "new-release",
-  "opera",
-  "pagode",
-  "party",
-  "philippines-opm",
-  "piano",
-  "pop",
-  "pop-film",
-  "post-dubstep",
-  "power-pop",
-  "progressive-house",
-  "psych-rock",
-  "punk",
-  "punk-rock",
-  "r-n-b",
-  "rainy-day",
-  "reggae",
-  "reggaeton",
-  "road-trip",
-  "rock",
-  "rock-n-roll",
-  "rockabilly",
-  "romance",
-  "sad",
-  "salsa",
-  "samba",
-  "sertanejo",
-  "show-tunes",
-  "singer-songwriter",
-  "ska",
-  "sleep",
-  "songwriter",
-  "soul",
-  "soundtracks",
-  "spanish",
-  "study",
-  "summer",
-  "swedish",
-  "synth-pop",
-  "tango",
-  "techno",
-  "trance",
-  "trip-hop",
-  "turkish",
-  "work-out",
-  "world-music"
-].join(", ")
-
 import OpenAI from 'openai';
-// let mood = "chilling in room doing work"
-// let num = 4 // slider implementation
 
 export async function getSuggestions(client: OpenAI, genres: Array<string>, mood: string, num: Number) {
-  let prompt = `The mood "${mood}" is best described by which ${num} of the following genres? Select exactly ${num} that are within the list, and respond with the answers only, separate by spaces. ${genres}`
+  let prompt = `The mood "${mood}" is best described by which ${num} of the following genres? Select exactly ${num}, no more, no less, that are within the list, and respond with the answers only, separate by spaces. ${genres}`
   const stream = await client.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: prompt }],
@@ -144,4 +13,25 @@ export async function getSuggestions(client: OpenAI, genres: Array<string>, mood
   }
   let suggestions = result.split(" ")
   return suggestions
+}
+
+export async function getNameDescription(client: OpenAI, genres: Array<string>, mood: string) {
+  let prompt = `"Develop a playlist name that blends the mood '${mood}' with the emotional tones of genres ${genres}, ensuring that the name retains some words from the original mood. The name should reflect both the mood and the genres. Follow the format: [Creative Name]:[Up to 3 Notable Genres] ["Playlist" or "Mix"]. Avoid special characters and quotations other than a single colon and follow proper spacing and capitalization conventions. Exactly one line after, write a brief description for the playlist that is congruent with previous details.`
+  const stream = await client.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      stream: true,
+  });
+  let result = "";
+  for await (const chunk of stream) {
+      result += chunk.choices[0]?.delta?.content || '';
+  }
+  const lines = result.split("\n").filter(line => line != '')
+  const playlistName = lines[0]
+  const playlistDescription = lines[1]
+  console.log(lines)
+  return {
+    playlistName,
+    playlistDescription
+  }
 }
--- src/routes/generate/+page.svelte
+++ src/routes/generate/+page.svelte
@@ -1,44 +1,160 @@
 <script lang="ts">
-  // import SpotifyWebApi from 'spotify-web-api-node';
-  // import OpenAI from 'openai';
-  // import { getSuggestions } from '../../api/gpt.js';
-  // import { beforeUpdate, onMount } from 'svelte';
-  // import { page } from '$app/stores';
+  import SpotifyWebApi from 'spotify-web-api-node';
+  import OpenAI from 'openai';
+  import { onMount } from 'svelte';
+  import { page } from '$app/stores';
+  import { getNameDescription, getSuggestions } from '../../api/gpt';
 
-  /** @type {import('./$types').PageData} */
-  // export let data;
-  // let accessToken = '';
-  // onMount(async () => {
-  //   // console.log($page.url.searchParams.get('code'))
-  //   const accessTokenRes = await fetch(`http://localhost:8080/api/spotify_access_token/${$page.url.searchParams.get('code')!}`)
-  //   const accessToken = await accessTokenRes.text();
-  //   console.log(accessToken)
-  //   const client = new SpotifyWebApi();
-  //   client.setAccessToken(accessToken);
+  const client = new SpotifyWebApi();
+  let openai: OpenAI;
+  onMount(async () => {
+    const accessTokenRes = await fetch(`http://localhost:8080/api/spotify_access_token/${$page.url.searchParams.get('code')!}`)
+    const accessToken = await accessTokenRes.text();
+    console.log(accessToken)
+    client.setAccessToken(accessToken);
   //   client.getAvailableGenreSeeds().then(async res => {
-  //     const gptKeyRes = await fetch(`http://localhost:8080/api/gpt_key`)
-  //     const gptKey = await gptKeyRes.text();
-  //     console.log(res)
+    const gptKeyRes = await fetch(`http://localhost:8080/api/gpt_key`)
+    const gptKey = await gptKeyRes.text();
     
-  //     const openai = new OpenAI({ apiKey: gptKey, dangerouslyAllowBrowser: true });
-  //     const suggestions = await getSuggestions(openai, res.body.genres, 'chilling in room doing work', 4);
-  //     console.log(suggestions)
-  //     client.getRecommendations({
-  //       seed_genres: suggestions,
-  //     }).then(res => {
-  //       console.log(res.body.tracks)
-  //     })
-  //   })
-  // })
+    openai = new OpenAI({ apiKey: gptKey, dangerouslyAllowBrowser: true });
+      //   })
+  });
+      
+  let playlistName = '';
+  let mood = '';
+  let playlistNumSongs = 55;
+  let playlistNewPercent = 0.75;
+  let playlistNumGenres = 3;
+  let playlistLink = '';
 
-  // client.createPlaylist('Playlist', {
-  //   description: 'This playlist was made using playlist-generator'
-  // }).then(() => {
-  //   console.log('wow')
-  // })
+  let generated = false;
+      
+  async function generate() {
+    // get all genres
+    const genres = (await client.getAvailableGenreSeeds()).body.genres;
+    // generate genre suggestions
+    const suggestions = await getSuggestions(openai, genres, mood, playlistNumGenres);
+    console.log('suggestions:', suggestions)
 
-  
+    // generate name and description
+    const playlistText = await getNameDescription(openai, suggestions, mood)
+    if (playlistName == '') playlistName = playlistText.playlistName;
+    
+    // create playlist
+    const playlist = (await client.createPlaylist(playlistName, { description: playlistText.playlistDescription })).body;
+    await client.changePlaylistDetails(playlist.id, { public: true });
+    console.log('playlist id:', playlist.id, playlist.public);
+    
+    // add tracks to playlist
+    const playlistNumNewSongs = Math.ceil(playlistNewPercent * playlistNumSongs)
+    const newTracks = (await client.getRecommendations({ seed_genres: suggestions, limit: playlistNumNewSongs })).body.tracks;
+    const likedTracks = (await client.getMySavedTracks({ limit: (playlistNumSongs - playlistNumNewSongs) })).body.items;
+    const tracks = newTracks.map(track => `spotify:track:${track.id}`).concat(likedTracks.map(track => `spotify:track:${track.track.id}`));
+    await client.addTracksToPlaylist(playlist.id, shuffle(tracks));
+
+    playlistLink = `https://embed.spotify.com/playlist/${playlist.id}`;
+    generated = true;
+  }
+
+  function shuffle(array: string[]) { 
+    for (let i = array.length - 1; i > 0; i--) { 
+      const j = Math.floor(Math.random() * (i + 1)); 
+      [array[i], array[j]] = [array[j], array[i]]; 
+    } 
+    return array; 
+  }; 
+
 </script>
 
-<h1>Generate</h1>
+<header class="main-header">
+  <img src="logo.jpg" alt="Spotiflow Logo" class="logo">
+  <h1>Spotiflow</h1>
+</header>
+<div class="container">
+  <div class="left-half">
+    <h2 class="inputs-header">Enter Playlist Details</h2>
+    <div class="text-container">
+      <label class="input-label" for="playlist-name">
+        Playlist Name <span style="font-weight: 300;">(Optional)</span>
+        <span class="tooltip">?
+          <span class="tooltiptext">Enter a name for your playlist. If left empty, a relevant name will be provided.</span>
+        </span>
+      </label>
+      <input type="text" id="playlist-name" class="text-box" placeholder="e.g., Motivation Mix" bind:value={playlistName}>
+    </div>
+    <div class="text-container">
+      <label class="input-label" for="playlist-mood">
+        Playlist Mood
+        <span class="tooltip">?
+          <span class="tooltiptext">Enter the mood or vibe you want the playlist to reflect. This could be a specific event, feeling, aesthetic, or time of day. Examples include ‘Energetic Workout’, ‘Chill Tuesday Afternoon’, ‘Romantic Evening’, or ‘Cottagecore Picnic’.</span>
+        </span>
+      </label>
+      <input type="text" id="playlist-mood" class="text-box" placeholder="e.g., Chill Vibes, Energetic Workout" bind:value={mood}>
+    </div>
+    <div class="slider-container">
+      <label class="input-label slider-label" for="song-count">
+        Song Count
+        <span class="tooltip">?
+          <span class="tooltiptext">Select the number of songs you want in your playlist.</span>
+        </span>
+      </label>
+      <div class="slider-wrapper">
+        <input id="song-count" class="slider-range" type="range" min="10" max="100" bind:value={playlistNumSongs}>
+        <span class="slider-value">{playlistNumSongs}</span>
+      </div>
+    </div>
+    <div class="slider-container">
+      <label class="input-label slider-label" for="discovery-level">
+        Discovery Level
+        <span class="tooltip">?
+          <span class="tooltiptext">Select the percentage of new songs in your playlist. A lower value means more songs from your library, and a higher value means more new discoveries.</span>
+        </span>
+      </label>
+      <div class="slider-wrapper">
+        <input id="discovery-level" class="slider-range" type="range" bind:value={playlistNewPercent} min="0.5" max="1" step="0.01">
+        <span class="slider-value">{Math.round(playlistNewPercent * 100)}%</span>
+      </div>
+    </div>
+    <div class="slider-container">
+      <label class="input-label slider-label" for="genre-variety">
+        Genre Variety
+        <span class="tooltip">?
+          <span class="tooltiptext">Choose how many different genres you want to include in your playlist.</span>
+        </span>
+      </label>
+      <div class="slider-wrapper">
+        <input id="genre-variety" class="slider-range" type="range" bind:value={playlistNumGenres} min="1" max="5">
+        <span class="slider-value">{playlistNumGenres}</span>
+      </div>
+    </div>
+  </div>
+  <div class="right-half">
+    {#if !generated}
+      <img src="placeholder.jpg" alt="Placeholder" class="placeholder-image">
+      <div class="button" on:click={generate} on:keydown={generate} role="button" tabindex="0">
+        <span>Generate</span>
+        <div id="loader" class="ld ld-ring ld-spin" style="display: none"></div>
+      </div>
+    {:else}    
+        <iframe src={playlistLink} class="output-iframe" id="output-iframe" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" title="playlist"></iframe>
+        <a href="/playlist-generator"><div class="button" role="button" tabindex="0">
+          <span>Generate Again</span>
+          <div id="loader" class="ld ld-ring ld-spin" style="display: none"></div>
+        </div></a>
+    {/if}
+  </div>
+</div>
+
+<!-- <h1>Generate</h1>
+<form action="">
+  <label for="playlistName">Playlist Name</label>
+  <input type="text" id="playlistName" bind:value={playlistName} disabled={generated}/>
+  <label for="playlistName">Mood</label>
+  <input type="text" id="playlistName" bind:value={mood} disabled={generated}/>
+  {#if generated}
+    <a href="/playlist-generator"><button>Generate again</button></a>
+  {:else}
+    <input type="submit" on:click={generate} value="Generational">
+  {/if}
+</form> -->
 <!-- <p></p> -->
